0d56abcbd9bd00448449a1b7c94fc0bfb2c863de
jamasouk354 <jamasouk354@SCS-D-360QJ2>
2019-02-20 09:54:49 -0500

    progress

---
+++ BoxField/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxField
+{
+    class Box
+    {
+        //TODO Need color
+        public int x, y, size;
+
+        public Box (int _x, int _y, int _size)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+        }
+
+        //TODO Move Method
+    }
+}
--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -17,9 +17,10 @@ namespace BoxField
 
         //used to draw boxes on screen
         SolidBrush boxBrush = new SolidBrush(Color.White);
-        
-        //TODO - create a list to hold a column of boxes        
 
+        List<Box> boxesLeft = new List<Box>();
+        List<Box> boxesRight = new List<Box>();
+        int timer = 0;
 
         public GameScreen()
         {
@@ -32,6 +33,8 @@ namespace BoxField
         public void OnStart()
         {
             //TODO - set game start values
+            Box b1 = new Box(30, 30, 30);
+            boxesLeft.Add(b1);
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -65,17 +68,26 @@ namespace BoxField
         private void gameLoop_Tick(object sender, EventArgs e)
         {
             //TODO - update location of all boxes (drop down screen)
-
+            timer++;
+            if (timer == 10)
+            {
+                OnStart();
+                timer = 0;
+            }
+            foreach (Box b in boxesLeft) { b.y = b.y + 5; }
             //TODO - remove box if it has gone of screen
-
-            //TODO - add new box if it is time
-
+            if (boxesLeft[0].y == this.Height) { boxesLeft.RemoveAt(0); }
+            //TODO - add new box if it is time                        
             Refresh();
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             //TODO - draw boxes to screen
+            foreach (Box b in boxesLeft)
+            {
+                e.Graphics.FillRectangle(boxBrush, b.x, b.y, b.size, b.size);
+            }
         }
     }
 }
